6b7f825e26d0c0e75ed70e8bd6201ff1351d587e
Akshat Pareek <77152270+pareekakshat@users.noreply.github.com>
2021-05-12 22:03:34 +0530

    Add files via upload

---
+++ src/Mycomponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            THis is an about component
+            <p>lorem34</p>
+        </div>
+    )
+}
---
+++ src/Mycomponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Footer = () => {
+    let footerStyle={
+        
+        
+    }
+    return (
+        <footer className="bg-dark text-light py-3" style={footerStyle}>
+            <p className="text-center">
+                copyright &copy; MyTodosList.com
+            </p>
+                    </footer>
+    )
+}
